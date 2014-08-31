2bc79567a88a44df490b6faf4216061e6ea10410
marsprj <marsprj@gmail.com>
2014-08-31 11:11:30 +0800

    添加ribbon的事件响应函数

--- js/Control/Ribbon.js
+++ js/Control/Ribbon.js
@@ -5,42 +5,14 @@ MapCloud.Ribbon = MapCloud.Class({
 	ribbon : null,
 	
 	initialize : function(){
-
-		var mcribbon = this;
-		
+	
 		this.ribbon = $("#ribbon_wrapper").first();
 
 		this.hideAllRibbons();
-
-		$("#ribbon_tabs li").each(function() {
-						
-			$(this).mouseover(function(){
-				$(this).addClass("mc-theme-color-hover");
-			});
-			$(this).mouseout(function(){
-				$(this).removeClass("mc-theme-color-hover");
-			});			
-			$(this).click(function(){
-				var id =  $(this).attr("id");
-				var type = id.substr(0, id.length-4);
-				$("#ribbon_tabs li").each(function() {
-                    $(this).removeClass("mc-active-tab");					
-                });
-				$(this).addClass("mc-active-tab");
-				
-				mcribbon.hideAllRibbons();
-				mcribbon.showRibbon(type);
-			});
-        });
+		this.registerRibbonEvents();
 		
-		$(".ribbon-item").each(function() {
-			$(this).mouseover(function(){
-				$(this).addClass("ribbon-item-over");
-			});
-			$(this).mouseout(function(){
-				$(this).removeClass("ribbon-item-over");
-			});			
-		});
+		this.enableMneuEffect();
+		this.registerMenuEvents();
 	},
 	
 	destory : function(){
@@ -66,7 +38,131 @@ MapCloud.Ribbon = MapCloud.Class({
 	
 	isCollapsed : function(){
 		return (this.ribbon.css("height") == this.height_min);
-	}
+	},
 	
+	registerRibbonEvents : function(){
+	
+		var mcribbon = this;	
+		$("#ribbon_tabs li").each(function() {
+						
+			$(this).mouseover(function(){
+				$(this).addClass("mc-theme-color-hover");
+			});
+			$(this).mouseout(function(){
+				$(this).removeClass("mc-theme-color-hover");
+			});			
+			$(this).click(function(){
+				var id =  $(this).attr("id");
+				var type = id.substr(0, id.length-4);
+				$("#ribbon_tabs li").each(function() {
+                    $(this).removeClass("mc-active-tab");					
+                });
+				$(this).addClass("mc-active-tab");
+				
+				mcribbon.hideAllRibbons();
+				mcribbon.showRibbon(type);
+			});
+        });
+	},
+	
+	enableMneuEffect : function(){
+		$(".ribbon-item").each(function(index, element) {
+			$(this).mouseover(function(){
+				$(this).addClass("ribbon-item-over");
+			});
+			$(this).mouseout(function(){
+				$(this).removeClass("ribbon-item-over");
+			});	
+        });
+	},
+	
+	registerMenuEvents : function(){
+		var that = this;
+		$(".ribbon-item").each(function(index, element) {
+			$(this).click(function(e) {
+				switch(index){
+				// Map Events
+				case 0:
+					that.onMapNew();
+					break;
+				case 1:
+					that.onMapPropertis();
+					break;
+				// Layer Events
+				case 2:
+					that.onLayerNew();
+					break;
+				case 3:
+					that.onLayerAddVector();
+					break;
+				case 4:
+					that.onLayerAddWMS();
+					break;
+				case 5:
+					that.onLayerAddWFS();
+					break;
+				// Data Events
+				case 6:
+					that.onFile();
+					break;
+				case 7:
+					that.onDatabase();
+					break;
+				case 8:
+					that.onDataImport();
+					break;
+				};
+			});
+        });
+	},
+
+	/**************************************************************/
+	/* Map Event                                                  */
+	/**************************************************************/
+	onMapNew : function(){
+		alert("onMapNew");
+	},
+	
+	onMapPropertis : function(){
+		alert("onMapPropertis");
+	},
+	
+	/**************************************************************/
+	/* Layer Event                                                */
+	/**************************************************************/
+	onLayerNew : function(){
+		alert("onLayerNew");
+	},
+	
+	onLayerAddVector : function(){
+		alert("onLayerAddVector");
+	},
+	
+	onLayerAddRaster : function(){
+		alert("onLayerAddRaster");
+	},
+	
+	onLayerAddWMS : function(){
+		alert("onLayerAddWMS");
+	},
+	
+	onLayerAddWFS : function(){
+		alert("onLayerAddWFS");
+	},
+	
+	/**************************************************************/
+	/* Data Event                                                */
+	/**************************************************************/
+	onFile : function(){
+		alert("onFile");
+	},
+	
+	onDatabase : function(){
+		alert("onDatabase");
+	},
+	
+	onDataImport : function(){
+		alert("onDataImport");
+	},
 });
 	
