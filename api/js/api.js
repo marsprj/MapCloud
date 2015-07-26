287847f6b5bab7e463685d99aa0362a1de660c61
marsprj <marsprj@gmail.com>
2015-07-26 20:17:07 +0800

    修改api页面样式

--- api/js/api.js
+++ api/js/api.js
@@ -20,16 +20,21 @@ function loadCatalog(){
 	var html  = "";
 	var count = g_catalog.length;
 	for(var i=0; i<count; i++){
-		html += "<div class='service_container'>";
+		html += "<div class='service_container' scroll='no'>";
 
 		var catalog = g_catalog[i];
-		html += "<div class='service_title'>" + catalog.name + "</div>";
+		//html += "<div class='service_title'>" + catalog.name + "</div>";
+		html += "<div class='service_title'><div class='service_title_icon'></div><span>" + catalog.name + "</span></div>";
 		html += "<div class='item_container' id='item_" + i.toString() + "'>";
 		var items = catalog.items.length;
 		for(var j=0; j<items; j++){
 
 			var item = catalog.items[j];
-			html += "<div class='item_block' onclick='onItemClick(\"" + item.name + "\",\"" + item.link + "\");'>" + item.name + "</div>";
+			//html += "<div class='item_block' onclick='onItemClick(\"" + item.name + "\",\"" + item.link + "\");'>" + item.name + "</div>";
+			html += "<div class='item_block' onclick='onItemClick(\"" + item.name + "\",\"" + item.link + "\");'>";
+			html += "<div class='item_block_icon'></div>";
+			html += "<span>" + item.name + "<span>";
+			html += "</div>";
 		}
 
 		html += "</div>";
