b380522a1b9ec5ad38bf186d7f1bb9bcf150d4e8
marsprj <marsprj@gmail.com>
2015-08-06 10:51:56 +0800

    MapCloud Add console pages

---
+++ console/js/catalog.js
@@ -0,0 +1,102 @@
+var g_catalog = 
+[
+    {
+        "name": "用户管理",
+        "items": [
+            {
+                "name": "用户管理",
+                "link": "mc_user.html"
+            },
+            {
+                "name": "角色管理",
+                "link": "wfs_describe_featuretype.html"
+            }
+        ]
+    },
+    {
+        "name": "数据库管理",
+        "items": [
+            {
+                "name": "PostgreSQL",
+                "link": "wmts_getcapabilities.html"
+            },
+            {
+                "name": "MongoDB",
+                "link": "wmts_get_tile.html"
+            },
+            {
+                "name": "Riak",
+                "link": "wmts_get_tile.html"
+            }
+        ]
+    },
+    {
+        "name": "服务管理",
+        "items": [
+            {
+                "name": "GetCapabilities",
+                "link": "wmts_getcapabilities.html"
+            },
+            {
+                "name": "GetTile",
+                "link": "wmts_get_tile.html"
+            }
+        ]
+    },
+    {
+        "name": "数据管理",
+        "items": [
+            {
+                "name": "矢量数据",
+                "link": "wms_getcapabilities.html"
+            },
+            {
+                "name": "影像数据",
+                "link": "wms_getcapabilities.html"
+            },
+            {
+                "name": "瓦片数据",
+                "link": "wms_getcapabilities.html"
+            },
+            {
+                "name": "专题数据",
+                "link": "wms_getcapabilities.html"
+            }
+        ]
+    },
+    {
+        "name": "开发文档",
+        "items": [
+            {
+                "name": "GetCapabilities",
+                "link": "wmts_getcapabilities.html"
+            },
+            {
+                "name": "GetTile",
+                "link": "wmts_get_tile.html"
+            }
+        ]
+    },
+    {
+        "name": "系统监控",
+        "items": [
+            {
+                "name": "GetCapabilities",
+                "link": "gps_getcapabilities.html"
+            },
+            {
+                "name": "GetArea",
+                "link": "gps_get_area.html"
+            }
+        ]
+    },
+    {
+        "name": "系统参数",
+        "items": [
+            {
+                "name": "GetCapabilities",
+                "link": "ims_getcapabilities.html"
+            }
+		]
+	}
+];
