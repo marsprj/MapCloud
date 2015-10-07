898a86eaed799e3b607351f19c9444392fa38658
marsprj <marsprj@gmail.com>
2015-10-07 21:46:58 +0800

    新建MapUser工程

---
+++ MapUser/js/mc-user-catalog.js
@@ -0,0 +1,57 @@
+var g_catalog = 
+[
+    {
+        "name": "用户信息",
+        "items": [
+            {
+                "name": "用户信息",
+                "link": "user_info.html"
+            }
+        ]
+    },
+    {
+        "name": "服务管理",
+        "items": [
+            {
+                "name": "服务管理",
+                "link": "wms_getcapabilities.html"
+            }
+        ]
+    },
+    {
+        "name": "地图管理",
+        "items": [
+            {
+                "name": "地图管理",
+                "link": "wms_getcapabilities.html"
+            }
+        ]
+    },
+    {
+        "name": "文件管理",
+        "items": [
+            {
+                "name": "文件管理",
+                "link": "user_data_fs.html"
+            }
+        ]
+    },
+    {
+        "name": "影像数据",
+        "items": [
+            {
+                "name": "影像数据",
+                "link": "gps_getcapabilities.html"
+            }
+        ]
+    },
+    {
+        "name": "瓦片数据",
+        "items": [
+            {
+                "name": "瓦片数据",
+                "link": "ims_getcapabilities.html"
+            }
+		]
+	}
+];
