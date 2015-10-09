3dd9cb7dfc437337e1d768b4b988948a76d737f1
fanzia <caisefan@163.com>
2015-10-09 11:20:38 +0800

    修改DEM部分的后缀设置。

--- js/Control/Dialog/GPS/GPSDemHillshadeDialog.js
+++ js/Control/Dialog/GPS/GPSDemHillshadeDialog.js
@@ -153,6 +153,12 @@ MapCloud.GPSDemHillshadeDialog = MapCloud.Class(MapCloud.Dialog,{
 		this.panel.find(".gps-input-source-name").val(this.inputSourceName);
 		this.panel.find(".gps-input-raster-name").val(this.inputRasterName);
 		this.panel.find(".gps-input-raster-path").val(this.inputRasterPath);
+
+
+		// 设置输出后缀
+		var index = this.inputRasterName.lastIndexOf(".");
+		var format = this.inputRasterName.slice(index,this.inputRasterName.length);
+		this.panel.find(".gps-output-raster-format").val(format);
 	},
 	
 	// 输出参数
